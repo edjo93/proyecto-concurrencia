4d7d8b2164e4baf94cdafd9784606361137a5485
Mayra Salazar <mayramegaten88@gmail.com>
2020-12-19 22:51:44 -0600

    starting react-web3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  componentWillMount(){
+    this.loadBlockChainData()
+    console.log(this.state.account);
+  }
+
+  async loadBlockChainData(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network  = await web3.eth.net.getNetworkType();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account: accounts[0]});
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {account: ''}
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello World!
+          </p>
+          <p>Your account:{this.state.account} </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
