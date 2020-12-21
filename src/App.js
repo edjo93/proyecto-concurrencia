04e62771fb194173a9a990554504f7d7a5ce7d3a
Mayra Salazar <mayramegaten88@gmail.com>
2020-12-21 01:23:22 -0600

    passing data

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import AddAlbum from './Components/Albums/AddAlbum.js';
 import Login from './Components/Login/Login.js';
 import Footer from './Components/Footer/Footer.js';
 import Home from './Components/Home/Home.js'
+import AlbumContract from './contracts/Album.json';
 import { Nav } from 'react-bootstrap';
 
 
@@ -17,13 +18,16 @@ import { Nav } from 'react-bootstrap';
 
 // eslint-disable-next-line 
 class App extends Component {
-  state = {web3: null, accounts: null, contract: null };
+  state = {web3: null, accounts: null, contract: null, albumAdresses:[]};
 
+  addAlbumAddresses = async (addressAdd)=>{
+    this.state.albumAdresses.push(addressAdd)
+  }
+  
   componentDidMount = async () => {
     try {
       // Get network provider and web3 mainInstance.
       const web3 = await getWeb3();
-
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
@@ -49,16 +53,20 @@ class App extends Component {
   };
 
   initiate = async () => {
-    const { accounts, contract, web3 } = this.state;
+    const { accounts, contract, web3,albumAdresses } = this.state;
     console.log("account: ", accounts[0]);
     console.log("methods", contract.methods);
-    // await contract.methods.setData("War", "Demon Hunter", "https://upload.wikimedia.org/wikipedia/en/c/cb/DemonHunter_War.jpg").send({ from: accounts[0] });
-    let response  = await contract.methods.album().call();
-    let newInstance = new web3.eth.Contract(AlbumContract.abi)
-    await newInstance.contract.methods.setData("Peace", "Demon Hunter", "https://target.scene7.com/is/image/Target/GUEST_8eb42664-16dc-4f03-9e71-dadadbf1abe4?wid=488&hei=488&fmt=pjpeg")
-    let response2 = await newInstance.contract.album().call();
-    console.log("contract: ", response);
-    console.log("contract2: ", response2);
+    // // await contract.methods.setData("War", "Demon Hunter", "https://upload.wikimedia.org/wikipedia/en/c/cb/DemonHunter_War.jpg").send({ from: accounts[0] });
+    // let response  = await contract.methods.album().call();
+    // // Creating new contracts
+    // //let newInstance = await new web3.eth.Contract(AlbumContract.abi).deploy({data: AlbumContract.bytecode}).send({"from":accounts[0]});
+    // let newInstance = await new web3.eth.Contract(AlbumContract.abi,"0x5f5518D4e8a4AAe8eab848f1999d9858067619E1");
+    // console.log(newInstance);
+    // // await newInstance.methods.setData("Peace", "Demon Hunter", "https://target.scene7.com/is/image/Target/GUEST_8eb42664-16dc-4f03-9e71-dadadbf1abe4?wid=488&hei=488&fmt=pjpeg").send({"from":accounts[0]});
+    // let response2 = await newInstance.methods.album().call();
+    // // console.log("contract: ", response);
+
+    console.log(albumAdresses);
   };
 
 
@@ -72,7 +80,7 @@ class App extends Component {
           <Switch>
             <Home path="/home" />
             <Albums path="/Albums" />
-            <AddAlbum path="/AddAlbum" />
+            <AddAlbum path="/AddAlbum" addressesCall={this.addAlbumAddresses.bind(this)}/>
             <Login path="/login" />
           </Switch>
           <Footer path="/" />
